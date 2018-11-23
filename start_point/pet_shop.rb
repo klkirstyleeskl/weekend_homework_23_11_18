25fc4e617ff899d872d733ffb89ab0c50aa0b0a3
klkirstyleeskl <k.lees@btinternet.com>
2018-11-23 12:57:09 +0000

    tries first test

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -1,1 +1,31 @@
+#return pet shop name
+def pet_shop_name (shop, name)
+  return pet_shop_name[:name]
+end
 
+# return total cash
+
+
+#return total cash add
+
+#return total cash remove
+
+#return pets sold
+
+#return stock count
+
+#return find pet by name
+
+#return remove pet by name
+
+#return add pet to stock
+
+#return test customer cash
+
+#return remove customer cash
+
+#return customer pet count
+
+#return add pet to customer
+
+#extra
