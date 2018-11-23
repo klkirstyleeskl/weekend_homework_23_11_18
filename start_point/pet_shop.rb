5f2ca3a9df7d33725e209451192eb4109b165f6c
klkirstyleeskl <k.lees@btinternet.com>
2018-11-23 13:50:10 +0000

    passes number of pets sold

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -5,17 +5,23 @@ end
 
 # return total cash
 def total_cash (cash)
-  return cash[@pet_shop][:admin][:total_cash]
+  return cash[:admin][:total_cash]
 end
 
 #return total cash add
-def total_cash_add
-
+def add_or_remove_cash(cash, amount)
+  #get current value
+  current_cash = cash[:admin][:total_cash]
+  # set new value
+  new_cash_value = current_cash + amount
+# store new value
+  cash[:admin][:total_cash] = new_cash_value
 end
 
-#return total cash remove
-
 #return pets sold
+def pets_sold (number_of_pets_sold)
+  return number_of_pets_sold[:admin][:pets_sold]
+end
 
 #return stock count
 
